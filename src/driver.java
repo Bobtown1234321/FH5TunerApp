be660efc0462cba654bea76f8a99548d5ac8a39d
Bobtown1234321 <119628238+Bobtown1234321@users.noreply.github.com>
2026-01-13 20:16:38 -0600

    Added Starting Indexes in the SampleData.txt

--- src/driver.java
+++ src/driver.java
@@ -1,16 +1,33 @@
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.text.DecimalFormat;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class driver {
     public static void main(String[] args) {
-        int port = 5001; //Port set in Forza
+        //saveAPI saveAPI= new saveAPI();
 
+        byte[] test = {102, 3, 0, 0, -122, -33, -7, 62};
+        //byte[] maxRpm = {-78, 77, 54, 57,};
+        //ByteBuffer buffer1 = ByteBuffer.wrap(maxRpm);
+        //buffer1.order(ByteOrder.LITTLE_ENDIAN);
+
+        ByteBuffer buffer = ByteBuffer.wrap(test);
+        buffer.order(ByteOrder.LITTLE_ENDIAN);
+        int index = buffer.getInt();
+        float rpm = buffer.getFloat();
+        DecimalFormat test1 = new DecimalFormat("0.00");
+
+        System.out.println(index);
+        System.out.println(test1.format(rpm));
+    }
+
+    protected void client(){
+        int port = 5001; //Port set in Forza
         ForzaClient client = new ForzaClient(port);
 
-        //saveAPI saveAPI= new saveAPI();
         ExecutorService service = Executors.newFixedThreadPool(1);
         service.submit(client);
-
-
     }
 }
